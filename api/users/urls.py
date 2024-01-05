2e21d80fb7d72966588c6ea1a05d6c903c2f0fda
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-01-05 04:36:42 -0500

    chore: adds new route for refresh token endpoint

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import UserLogin, UserRegistration
+from .views import UserLogin, UserRegistration, TokenRefresh
 
 urlpatterns = [
      path('users/login', UserLogin.as_view(), name='UserRegistration'),
-     path('users/signup', UserRegistration.as_view(), name='UserLogin')
+     path('users/signup', UserRegistration.as_view(), name='UserLogin'),
+     path('users/refresh-token', TokenRefresh.as_view(), name='TokenRefresh')
 ]
-   
