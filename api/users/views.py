c439cf368439127a969482fc7716567148cc75d8
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-01-05 04:37:17 -0500

    feat: adds view for refresh token

--- api/users/views.py
+++ api/users/views.py
@@ -1,4 +1,6 @@
 import json
+import jwt
+from django.conf import settings
 from django.contrib.auth.hashers import make_password, check_password
 from django.views.decorators.http import require_http_methods
 from django.utils.decorators import method_decorator
@@ -43,3 +45,26 @@ class UserLogin(View):
                 return JsonResponse({'response': {'access_token': token, 'refresh_token': refresh_token}}, status=200)
         return JsonResponse({'response': 'wrong user or password'}, status=401)
         
+
+class TokenRefresh(View):
+
+    @method_decorator(require_http_methods(['POST']))
+    def post(self, request):
+        refresh_token = request.headers.get('Authorization', '').split(' ')[-1]
+
+        try:
+            decoded_payload = jwt.decode(refresh_token, settings.REFRESH_TOKEN_KEY, algorithms=['HS256'])
+            token_type = decoded_payload.get('token_type')
+        except jwt.ExpiredSignature:
+            return JsonResponse({'response': 'token expired'}, status=401)
+
+        except jwt.InvalidTokenError:
+            return JsonResponse({'response': 'invalid access token'}, status=401)
+        
+        if token_type != 'refresh_token':
+            return JsonResponse({'response': 'wrong token type'}, status=401)
+        
+        user_id = decoded_payload.get('user_id')
+        new_access_token = create_jwt(user_id)
+
+        return JsonResponse({'response': {'new_access_token': new_access_token}}, status=200)
