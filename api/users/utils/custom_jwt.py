8579796e8b6da07bfe200faf71d27c23ef4dd8a7
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-01-05 23:47:45 -0500

    chore: adds pep8 format

--- api/users/utils/custom_jwt.py
+++ api/users/utils/custom_jwt.py
@@ -2,18 +2,20 @@ from datetime import datetime, timedelta
 import jwt
 from django.conf import settings
 
+
 def create_jwt(user_id: int):
     payload = {
         'user_id': str(user_id),
         'token_type': 'access_token',
         'exp': datetime.utcnow() + timedelta(minutes=1),
-        'iat': datetime.utcnow()      
+        'iat': datetime.utcnow()
     }
-    
-    secret_key = settings.JWT_SECRET_KEY  
+
+    secret_key = settings.JWT_SECRET_KEY
     token = jwt.encode(payload, secret_key, algorithm='HS256')
-    
+
     return token
+
 
 def create_refresh_token(user_id: int):
     refresh_payload = {
@@ -22,8 +24,8 @@ def create_refresh_token(user_id: int):
         'exp': datetime.utcnow() + timedelta(minutes=6),
         'iat': datetime.utcnow()
     }
-    
+
     secret_key = settings.REFRESH_TOKEN_KEY
     refresh_token = jwt.encode(refresh_payload, secret_key, algorithm='HS256')
-    
+
     return refresh_token
--- api/users/views.py
+++ api/users/views.py
@@ -9,42 +9,45 @@ from django.http import JsonResponse
 from django.views import View
 from .utils.custom_jwt import create_jwt, create_refresh_token
 
+
 class UserRegistration(View):
     @method_decorator(require_http_methods(['POST']))
     def post(self, request):
-        
+
         request_data = json.loads(request.body)
         username = request_data.get('username')
         password = request_data.get('password')
-        
+
         if username and password:
-            new_user = User(username=username, password=make_password(password))
+            new_user = User(username=username,
+                            password=make_password(password))
             new_user.save()
-            
+
             return JsonResponse({'response': 'user created'}, status=201)
         return JsonResponse({'response': 'missing required data'}, status=400)
-        
+
+
 class UserLogin(View):
     @method_decorator(require_http_methods(['POST']))
     def post(self, request):
-        
+
         request_data = json.loads(request.body)
         username = request_data.get('username')
-        
+
         try:
-            current_user =  User.objects.get(username=username)
+            current_user = User.objects.get(username=username)
         except User.DoesNotExist:
             return JsonResponse({'response': 'wrong user or password'}, status=404)
-        
+
         password = request_data.get('password')
-        
+
         if current_user is not None:
             if check_password(password, current_user.password):
                 token = create_jwt(current_user.id)
                 refresh_token = create_refresh_token(current_user.id)
                 return JsonResponse({'response': {'access_token': token, 'refresh_token': refresh_token}}, status=200)
         return JsonResponse({'response': 'wrong user or password'}, status=401)
-        
+
 
 class TokenRefresh(View):
 
@@ -53,17 +56,18 @@ class TokenRefresh(View):
         refresh_token = request.headers.get('Authorization', '').split(' ')[-1]
 
         try:
-            decoded_payload = jwt.decode(refresh_token, settings.REFRESH_TOKEN_KEY, algorithms=['HS256'])
+            decoded_payload = jwt.decode(
+                refresh_token, settings.REFRESH_TOKEN_KEY, algorithms=['HS256'])
             token_type = decoded_payload.get('token_type')
         except jwt.ExpiredSignature:
             return JsonResponse({'response': 'token expired'}, status=401)
 
         except jwt.InvalidTokenError:
             return JsonResponse({'response': 'invalid access token'}, status=401)
-        
+
         if token_type != 'refresh_token':
             return JsonResponse({'response': 'wrong token type'}, status=401)
-        
+
         user_id = decoded_payload.get('user_id')
         new_access_token = create_jwt(user_id)
 
