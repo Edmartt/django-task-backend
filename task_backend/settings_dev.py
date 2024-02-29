968e0f12cbed47da0268ca716953e2630611305e
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-02-29 04:06:48 -0500

    chore: adds refresh token key and fix typo

--- task_backend/settings_dev.py
+++ task_backend/settings_dev.py
@@ -3,9 +3,10 @@ from .settings import *
 
 DEBUG = True
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY') or '1234'
+REFRESH_TOKEN_KEY = os.getenv('REFRESH_TOKEN_KEY') or '1234'
 DATABASES = {
         'default':{
-            'ENGINE': 'django.bd.backends.sqlite3',
+            'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3'
             }
         }
